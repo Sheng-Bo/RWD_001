8cc632eacb3eeb4568f35a3a2c4b30def3eb9bbe
Sheng-Bo Chen <artalose@Sheng-BodeMacBook-Pro.local>
2016-05-26 04:58:07 -0400

    2.0.1

--- sidebar.js
+++ sidebar.js
@@ -1,37 +1,29 @@
 $(function () {
 
-
-
 	function toggle_sidebar() {
 
 		var viewport_width = $(window).width();
 
 		if (viewport_width < 1025) {
-
-			$('.wrapper > .sidebar').removeClass('open').removeClass('close');
-			$('.wrapper > .main').removeClass('open').removeClass('close');
-
 			$('.hamburger').unbind('click').click(function () {
-				$('.wrapper > .sidebar').toggleClass('open');
-				$('.wrapper > .main').toggleClass('open');
+				$('.sidebar').toggleClass('open');
+				$('.main').toggleClass('open');
+				$('.main > .header').toggleClass('open');
 			});
 		} // end if
 
 		if (viewport_width >= 1025) {
-
-			$('.wrapper > .sidebar').removeClass('open').removeClass('close');
-			$('.wrapper > .main').removeClass('open').removeClass('close');
-
+			$('.sidebar').removeClass('open');
+			$('.main').removeClass('open');
+			$('.main > .header').removeClass('open');
 		} // end if
-
 	} // end function
-
 
 	toggle_sidebar();
 
-
-	$(window).resize(function () {
-		toggle_sidebar();
-	});
+	$(window)
+		.resize(function () {
+			toggle_sidebar();
+		});
 
 });
